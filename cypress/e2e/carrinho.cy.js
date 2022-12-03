83834aaecef96d65e6a90a12ecce9e0ecfce4f6b
Mirlanda Costa <mirlanda.dsc@gmail.com>
2022-12-03 01:25:27 -0400

    Testes Adicionar produto no carrinho

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -1,9 +1,27 @@
-const cypress = require("cypress")
-
 describe('Carrinho de compras', () => {
 
     beforeEach(()=>{
-        cypress.visit('https://www.amazon.com.br/')
+        cy.visit('https://www.amazon.com.br/')
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+          })
+    })
+
+    it('Adicionar produto no carrinho', () => {
+        cy.wait(1000)
+        cy.get('#twotabsearchtextbox').type('fone de ouvido PHILIPS sem fio TWS');
+        cy.get('#nav-search-submit-button').click()
+        cy.scrollTo(0,300)
+
+        cy.get('[data-asin="B09XXRC59V"]').click()
+        cy.wait(500)
+        cy.get('#breadcrumb-back-link').should('be.visible');
+
+        cy.get('#add-to-cart-button').click()
+        cy.contains('Adicionar ao carrinho').should('be.visible');
     })
 
 
